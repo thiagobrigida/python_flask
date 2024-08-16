676ad33d9524c8f16b24fd765aa1d09de436f86e
thiagobrigida <thiago.brigi@gmail.com>
2024-08-16 08:36:59 -0300

    Aula de 16/08

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '23ttnd4gnd55y-23md98'
 
 @app.route('/')
 def index():
@@ -13,7 +14,16 @@ def aula(nome = 'João', curso = 'Informática', ano = 1):
     dados = {'nome':nome,'curso':curso, 'ano':ano}
     return render_template('aula.html',dados_curso = dados)
 
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+@app.route('/dados', methods=['POST'])
+def dados():
+    flash('Dados ')
+    dados = request.form
+    return render_template('dados.html', dados=dados)
+
 if __name__ == '__main__':
     app.run()
-    
-    
+
