2527c7134a4cbf855d9e345078bdcdc03074951c
thiagobrigida <thiago.brigi@gmail.com>
2024-08-23 08:41:12 -0300

    Aula de 23/08

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, flash
 app = Flask(__name__)
+from database import db
+from flask_migrate import Migrate
+from models import Usuario
 app.config['SECRET_KEY'] = '23ttnd4gnd55y-23md98'
+
+#drive://usuario:senha@servidor/banco_de_dados
+conexao = "mysql+pymysql://alunos:cefetmg@127.0.0.1/alunos"
+app.config['SQLALCHEMY_DATABASE_URI'] = conexao
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
@@ -23,6 +34,11 @@ def dados():
     flash('Dados ')
     dados = request.form
     return render_template('dados.html', dados=dados)
+
+@app.route('/usuario')
+def usuario():
+    u = Usuario.query.all()
+    return render_template('usuario_lista.html', dados = u)
 
 if __name__ == '__main__':
     app.run()
